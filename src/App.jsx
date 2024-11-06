33f417275bc89a6c300e3352f16f66bc8ddc8a75
Lily <alcarrillomunoz@gmail.com>
2024-11-06 12:14:26 -0500

    TodoList and AddTodoForm components created

--- src/App.jsx
+++ src/App.jsx
@@ -1,32 +1,14 @@
 import { useState } from 'react'
 import './App.css'
-
-const todoList = [ 
-  {
-    id: 1, 
-    title: "Complete lesson materials",
-  },
-  {
-    id: 2,
-    title: "Attend group session",
-  },
-  {
-    id: 3,
-    title: "Complete assignment"
-  },
-];
+import TodoList from './TodoList'
+import AddTodoForm from './AddTodoForm'
 
 function App() {
   return (
       <div>
         <h1>Todo List</h1>
-        <ul>
-          {todoList.map(function (item) {
-            return (
-              <li key={item.id}>{item.title}</li>
-            )
-          })}
-        </ul>
+        <AddTodoForm />
+        <TodoList />
       </div>     
   )
 }
