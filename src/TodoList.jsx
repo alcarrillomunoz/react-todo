f0474e7a09a5fdbbecbbb05ef3d1c6de6ae3b14c
Lily <alcarrillomunoz@gmail.com>
2025-01-29 15:52:38 -0500

    CSS Styled Components SVG

--- src/TodoList.jsx
+++ src/TodoList.jsx
@@ -1,14 +1,18 @@
 import TodoListItem from "./TodoListItem";
 
-function TodoList({todoList, onRemoveTodo}) {
-    return (
-        <ul>
-        {todoList.map((item) => (     
-          <TodoListItem key={item.id} id={item.id} todo={item.title} onRemoveTodo={onRemoveTodo}/>
-           )
-        )} 
-      </ul> 
-    )
+function TodoList({ todoList, onRemoveTodo }) {
+  return (
+    <ul>
+      {todoList.map((item) => (
+        <TodoListItem
+          key={item.id}
+          id={item.id}
+          todo={item.title}
+          onRemoveTodo={onRemoveTodo}
+        />
+      ))}
+    </ul>
+  );
 }
 
-export default TodoList
+export default TodoList;
